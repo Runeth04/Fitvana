847a6712da886b70e97c842dfe05c7a2f4e55350
Runeth04 <it23324060@my.sliit.lk>
2025-04-01 22:10:31 +0530

    lkjhgfdsa

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv");
 const path = require("path");
 const app = express();
 require("dotenv").config();
-
+//Connect
 const PORT = process.env.PORT || 8070;
 app.use(cors());
 app.use(bodyParser.json());
